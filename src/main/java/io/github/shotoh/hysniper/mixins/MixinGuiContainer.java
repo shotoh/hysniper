5d963e2adfe732f8fb0b3d1da61cb09bd64a0bd9
shotoh <shotoh0806@gmail.com>
2023-07-29 12:05:11 -0700

    implemented a legit way of creating ghost blocks

--- src/main/java/io/github/shotoh/hysniper/mixins/MixinGuiContainer.java
+++ src/main/java/io/github/shotoh/hysniper/mixins/MixinGuiContainer.java
@@ -10,21 +10,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = GuiContainer.class, priority = 1100)
+@Mixin(value = GuiContainer.class, priority = -100)
 public class MixinGuiContainer {
     @Inject(method = "handleMouseClick", at = @At(value = "HEAD"), cancellable = true)
     public void handleMouseClick(Slot slotIn, int slotId, int clickedButton, int clickType, CallbackInfo ci) {
-        UChat.chat(1);
         if (slotIn == null) return;
-        UChat.chat(2);
         GuiContainer gui = (GuiContainer) (Object) this;
         MouseClickEvent event = new MouseClickEvent(gui, slotIn, slotId, clickedButton, clickType);
         MinecraftForge.EVENT_BUS.post(event);
-        UChat.chat(3);
         if (event.isCanceled()) {
-            UChat.chat(4);
             ci.cancel();
         }
-        UChat.chat(5);
     }
 }
