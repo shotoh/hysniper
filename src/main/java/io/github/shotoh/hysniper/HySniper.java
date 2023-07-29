c7022d5bc57474a6b437869f1992d37df616a1b6
shotoh <shotoh0806@gmail.com>
2023-07-29 11:24:39 -0700

    reset mod version

--- src/main/java/io/github/shotoh/hysniper/HySniper.java
+++ src/main/java/io/github/shotoh/hysniper/HySniper.java
@@ -24,7 +24,7 @@ import java.util.concurrent.ScheduledExecutorService;
 public class HySniper {
     public static final String MODID = "hysniper";
     public static final String NAME = "HySniper";
-    public static final String VERSION = "3.0.0";
+    public static final String VERSION = "1.0.0";
 
     private final Gson gson;
     private final ScheduledExecutorService pool;
