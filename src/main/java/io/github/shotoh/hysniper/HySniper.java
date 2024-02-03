ce953f36939d0b638d40966573cf6fd31f7c92e1
shotoh <trueshotoh@gmail.com>
2024-02-02 16:08:15 -0800

    updated version number

--- src/main/java/io/github/shotoh/hysniper/HySniper.java
+++ src/main/java/io/github/shotoh/hysniper/HySniper.java
@@ -22,7 +22,7 @@ import java.util.concurrent.TimeUnit;
 public class HySniper {
     public static final String MODID = "hysniper";
     public static final String NAME = "HySniper";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.3.0";
 
     public static HySniperConfig CONFIG;
     public static Gson GSON;
